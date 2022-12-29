a0183a2c431221e3919707f19938a651bb62551e
jpanig000 <jpanig00@estudiantes.unileon.es>
2022-12-29 16:46:14 +0100

    Cambios en libs.h

--- libs.h
+++ libs.h
@@ -1,12 +1,8 @@
-#ifndef LIBS
-
-    #define LIBS
-    #include <time.h>
-    #include <sys/types.h>
-    #include <unistd.h>
-    #include <signal.h>
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <sys/wait.h>
-
-#endif
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <pthread.h>
--- main.c
+++ main.c
@@ -4,5 +4,10 @@
 
 int main(int argc, char *argv[]) {
 
+    pthread_mutex_init(&mutexFile, NULL);
+    pthread_mutex_init(&mutexCustList, NULL);
+    pthread_mutex_init(&mutexSolicitudes, NULL);
+
+    pthread_cond_init(&condSolicitudes, NULL);
 
 }

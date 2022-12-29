f28b3c26f4241f2a2d1b455acaf7b2b0abd6588c
jpanig000 <jpanig00@estudiantes.unileon.es>
2022-12-29 16:46:00 +0100

    Variables globales

--- globalVariables.c
+++ globalVariables.c
@@ -6,3 +6,22 @@ struct customer {
     int solicited;  // 1 if they have solicited domiciliary attention
 };
 
+struct customer customerList[20];
+
+int appCustNum = 0;
+int netCustNum = 0;
+int domiciliarySolsNum = 0;
+
+pthread_mutex_t mutexFile;
+pthread_mutex_t mutexCustList;
+pthread_mutex_t mutexSolicitudes;
+
+pthread_cond_t condSolicitudes;
+
+struct technician
+{
+    pid_t pid;
+    char type;      // 'A' for app, 'N' for net
+};
+
+FILE logFile;

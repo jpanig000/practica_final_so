618d222792730712ebc8754c4d319943c700e0ac
sgonzr12 <sgonzr12@estudiantes.unileon.es>
2022-12-29 18:31:35 +0100

    añadida funcion principal e include de variables globales

--- accTecnico.c
+++ accTecnico.c
@@ -1,9 +1,17 @@
+#include "globalVariables.c"
+
+//principal function 
+void *accTecnico(struct technician tech){
+    techAction(tech);
+}
 
 //selects the acction to perform according to the tipe of the received technician
-void *techAction(struct technician tech){
-    if tech.type = 'A'{
+void techAction(struct technician tech){
+    if (tech.type == 'A'){
         appTech();
     }else{
         netTech();
     }
 }
+
+

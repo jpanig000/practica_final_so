fe917db8bac766337934dadc2a544171d96428c2
RubenFdezGlez <91256753+RubenFdezGlez@users.noreply.github.com>
2023-01-04 17:34:20 +0100

    Segunda version del tecnico domiciliario

--- accTecDom.c
+++ accTecDom.c
@@ -17,19 +17,54 @@ tecDom(){
     
     
     //Guardamos en el log que comienza la atencion
+    pthread_mutex_lock(&mutexFile);
+    
     writeLog("tecDom"," ha comenzado la atencion domiciliaria");
+    
+    pthread_mutex_unlock(&mutexFile);
+    
     
     for(int i = 0; i < 4; i++){
         
+        struct customer next;
+        next.id = 0;
+        
+        //Posicion para buscar en los clientes cuales esperan por la atencion
+        int pos = 0; 
+        
         pthread_mutex_lock(&mutexCustList);
+        
+        do{
+            struct customer actual = customerList[pos];
+            if(actual.solicited == 1){
+                next = actual;
+            }
+            
+        }while(next.id == 0);
+        
+        pthread_mutex_unlock(&mutexCustList);
+        
+        
         //Descansa un segundo por cada cliente
         sleep(1);
         
+        
+        //Escribe en el log que ha atendido al cliente
+        pthread_mutex_lock(&mutexFile);
+        
         writeLog(""," ha atendido al cliente");
         
+        pthread_mutex_unlock(&mutexFile);
+        
+        
         //Poner flag de solicitud a 0
+        pthread_mutex_lock(&mutexCustList);
+        
+        next.solicited == 1;
+        
         pthread_mutex_unlock(&mutexCustList);
     }
+    
     
     //Pone el contador de solicitudes a 0
     pthread_mutex_lock(&mutexDomRequest);
@@ -41,6 +76,12 @@ tecDom(){
     
     
     //Guardamos en el log que se ha finalizado la solicitud domiciliaria    
+    pthread_mutex_lock(&mutexFile);
+    
+    writeLog("tecDom"," ha finalizado la atencion domiciliaria");
+    
+    pthread_mutex_unlock(&mutexFile);
+    
     
     //Avisa a los que han terminado que la solicitud actual ha finalizado
     pthread_mutex_lock(&mutexDomRequest);

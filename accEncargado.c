57a3469d84c47737843303bb78a6ecfbb5e8f987
sgonzr12 <sgonzr12@estudiantes.unileon.es>
2023-01-04 19:11:23 +0100

    logs de accEncargado

--- accEncargado.c
+++ accEncargado.c
@@ -63,21 +63,57 @@ void manAction(){
                                                             //calcular numero aleatorio para tipo de atencion
     int atencion = 1; //numAleatorio(0-100);
     
-    //escribir en log que se inicia la atencion
+                                                            //escribir en log que se inicia la atencion
+
+        char id[100] = "man";
+    
+    char idCli[100] = "";
+
+    sprintf(idCli,"%d",next.id);
+
+    char message[] = "starts the attention to client ";
+
+    strcat(message, idCli);
+
+    pthread_mutex_lock(&mutexFile);
+        writeLog(id, message);
+    pthread_mutex_unlock(&mutexFile); 
                                                             
                                                             //dormir tiempo de atencion
+    int fin ;
     if(atencion > 0 && atencion < 10){
+        fin = 0;
         sleep(0);// numAleatorio(2-6));                     //error de identificacion
     }else if(atencion > 10 && atencion < 20){
+        fin = 1;
         sleep(0);// numAleatorio(1-2));                     //compañia equivocada
     }else if(atencion > 20 && atencion < 100){
+        fin = 2;
         sleep(0);// numAleatorio(1-4));                     //todo en regla
     }
     
-    //escribir en log que finaliza la atencion
+                                                            //escribir en log que finaliza la atencion
+    char message1[] = "finalized the attention to client ";
+    strcat(message1, idCli);
+
+    pthread_mutex_lock(&mutexFile);
+        writeLog(id, message1);
+    pthread_mutex_unlock(&mutexFile); 
+
+                                                            //escribir en log motivo de finalización
+    char message2[] = "the action finalized because ";
+    if(fin == 0){
+        strcat(message2, "it was an autentication error");
+    }else if(fin == 1){
+        strcat(message2, "the customer has the wrong company");
+    }else if(fin == 2){
+        strcat(message2, "the customer was atended correctly");
+    }
     
-    //escribir en log motivo de finalización
-    
+
+    pthread_mutex_lock(&mutexFile);
+        writeLog(id, message2);
+    pthread_mutex_unlock(&mutexFile);
                                                             //cambiar atendido
     pthread_mutex_lock(&mutexCustList);
         next.isAttended == 0;

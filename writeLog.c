5268656fd50e2d7c340e3a6b14575a69915a2c6e
RubenFdezGlez <91256753+RubenFdezGlez@users.noreply.github.com>
2023-01-04 15:09:07 +0100

    Metodo de escribir en el log

---
+++ writeLog.c
@@ -0,0 +1,17 @@
+//Escribe en el archivo con identificador id y mensaje
+void writeLog(char *id, char *msg){
+    time_t now = time(0);
+    struct tm *tlocal = localtime(&now);
+    char stnow[25];
+    strftime(stnow, 25, "%y/%m/%d %H:%M:%S", tlocal); 
+ 
+    // Escribimos en el log
+
+    logFile = fopen("log.txt", "a");
+    fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
+    fclose(logFile); 
+}
+
+
+
+

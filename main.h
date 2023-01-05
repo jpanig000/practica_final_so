e15a45add8bc2d44daa89f68894ffe7c60583344
jpanig000 <jpanig00@estudiantes.unileon.es>
2023-01-05 11:55:23 +0100

    Updates en main.h

--- main.h
+++ main.h
@@ -1,13 +1,17 @@
  
 #include "globalVariables.c"
-                                                           //Main
+
+//Main
 void declareHandlers();
 void newClientApp(int sig);
 void newClientNetwork(int sig);
-                                                            //nuevoCliente
+int calculaAleatorio(int min, int max);
+                                                            
+//nuevoCliente
 void finish(int sig);
 void nuevoCliente(char type);
-                                                            //accCliente
+
+//accCliente
 void *accCliente(void *ptr);
 void nameClient(int numClient, char nombre[]);
 int clientNotAttended(int numClient, int secondPassed);
@@ -16,17 +20,22 @@ void clientAttendedAndNetClient(int numClient);
 void clientDomiciliaryRequests(int numClient);
 void clientExit(int numClient, char* reason);
 void clearClient(int numClient);
-                                                            //accEncargado
+
+//accEncargado
 void *accEncargado();
 void manAction();
-                                                            //accTecnico
+                                                            
+//accTecnico
 void *accTecnico(void *tech);
 void techAction(struct technician tech);
-                                                            //accTecDom
+
+//accTecDom
 void *accTecnicoDomiciliario();
 void tecDom();
-                                                            //writeLog
+
+//writeLog
 void writeLog(char *id, char *msg);
+
 
 #include "accCliente.c"
 #include "nuevoCliente.c"

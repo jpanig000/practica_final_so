23f3593a3acc5af9fc1159f5fe4636bc7da0f4e1
sgonzr12 <sgonzr12@estudiantes.unileon.es>
2023-01-08 19:12:04 +0100

    comentarios

--- globalVariables.c
+++ globalVariables.c
@@ -3,15 +3,14 @@ struct customer {   // struct representing customers
     int isAttended; // -1 if is not, 0 if atending, 1 atended
     char type;      // 'A' for app, 'N' for net
     int priority;   
-    int solicited;  // -1 if the customer has the wrong company, 1 if they have solicited domiciliary attention
+    int solicited;  // -1 if the the customer cant solicity dom att, 1 if they have solicited domiciliary attention
 };
 
-struct customer *customerList;       // customers list
+struct customer customerList[20];       // customers list
 
 int appCustNum;     // type app customers counter
 int netCustNum;     // type net customers counter
-int domSolsNum;     // domiciliary requests counter
-int techAppNum;     // type app technicians counter
+int domSolsNum;     // domiciliary requests number
 
 pthread_mutex_t mutexFile;              // mutex for file access
 pthread_mutex_t mutexCustList;          // mutex for customer list access

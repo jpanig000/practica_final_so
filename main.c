88c20b514ed07ce6aba227700e8e9cf2196ad4f7
RubenFdezGlez <91256753+RubenFdezGlez@users.noreply.github.com>
2023-01-08 15:52:00 +0100

    free custlist

--- main.c
+++ main.c
@@ -172,6 +172,7 @@ void finish(int sig){                               //Finish the program
             }
         }  
     }while(numC == 1); 
+    free(customerList);
     kill(getpid(),SIGKILL);   
 }
 
